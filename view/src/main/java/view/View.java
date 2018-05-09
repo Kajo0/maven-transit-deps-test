f1959ef4f78a4893b29da8d1847a35381c16c3db
Mikolaj Markiewicz <kajo100@gmail.com>
2018-05-09 23:02:53 +0200

    Optional dependency

--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -1,7 +1,7 @@
 package view;
 
-import accessmanager.AccessManager;
-import com.google.common.collect.Lists;
+//import accessmanager.AccessManager;
+//import com.google.common.collect.Lists;
 import framework.Framework;
 
 public class View {
@@ -9,7 +9,7 @@ public class View {
     public static void main(String[] args) {
         System.out.println(new Framework().formatPhone("555333111"));
 
-        new AccessManager().sort(Lists.newArrayList(1, 2, 3), 1).forEach(System.out::println);
+//        new AccessManager().sort(Lists.newArrayList(1, 2, 3), 1).forEach(System.out::println);
     }
 
 }
